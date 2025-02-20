d5290cf572fc6184f7ebb580f4277f66905d6860
Zainb0408 <zainabfatima0408@hotmail.com>
2025-02-20 21:43:55 +0500

    python

---
+++ library.py
@@ -0,0 +1,15 @@
+import turtle
+screen= turtle.Screen()
+screen.setup(400,400)
+screen.bgcolor("purple")
+board=turtle.Turtle()
+board.forward(100)
+board.left(90)
+board.forward(100)
+board.left(90)
+board.forward(100)
+board.left(90)
+board.forward(100)
+
+
+turtle.done()
---
+++ python.py
@@ -0,0 +1,20 @@
+import turtle
+screen= turtle.Screen()
+screen.setup(400,400)
+screen.bgcolor("purple")
+board=turtle.Turtle()
+board.forward(70)
+board.left(60)
+board.forward(70)
+board.left(60)
+board.forward(70)
+board.left(60)
+board.forward(70)
+board.left(60)
+board.forward(70)
+board.left(60)
+board.forward(70)
+board.left(60)
+
+
+turtle.done()
